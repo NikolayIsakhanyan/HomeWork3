6937d525cd03c1a476f736cc2ca2d03711154e3b
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2022-12-03 11:19:06 +0400

    Add project files.

---
+++ ConsoleAppHomeWork3/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+
+        private static void Main(string[] args)
+        {
+        int a=Convert.ToInt32(Console.ReadLine());    
+        int b=Convert.ToInt32(Console.ReadLine());    
+        int c=Convert.ToInt32(Console.ReadLine());
+        //Max(a, b, c, 0);
+        //Max(b, a, c, 5);
+        }
+        //static void Max(int a, int b, int c,int max )
+        //{
+        //    if (a < b)
+        //    {
+        //         max= b;
+
+        //    }else {  max = a; }
+        //if (max < c)
+        //{
+        //    Console.WriteLine(c);
+        //}
+        //else { Console.WriteLine(max); }    
+        
+
+            
+        }
+    
+}
